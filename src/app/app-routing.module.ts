d2980f0e2271a3f5f43020d98eac5115df5ed5ed
sharique <ssharique38@gamil.com>
2023-12-24 03:14:46 +0530

    routing integrated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FellowshipComponent } from './fellowship/fellowship.component';
+import { OurWorkComponent } from './our-work/our-work.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'fellowship', component: FellowshipComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'ourwork', component: OurWorkComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurWorkComponent } from './our-work/our-work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FellowshipComponent } from './fellowship/fellowship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    OurWorkComponent,
+    ProjectsComponent,
+    FellowshipComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
