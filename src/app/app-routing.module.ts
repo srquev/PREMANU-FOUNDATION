75250a138fbf845a2333a1209adfbfc0b21652df
sharique <ssharique38@gamil.com>
2023-12-24 03:28:18 +0530

    components wrapped up into one folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { FellowshipComponent } from './fellowship/fellowship.component';
-import { OurWorkComponent } from './our-work/our-work.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FellowshipComponent } from './components/fellowship/fellowship.component';
+import { OurWorkComponent } from './components/our-work/our-work.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { OurWorkComponent } from './our-work/our-work.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { FellowshipComponent } from './fellowship/fellowship.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { OurWorkComponent } from './components/our-work/our-work.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FellowshipComponent } from './components/fellowship/fellowship.component';
 
 @NgModule({
   declarations: [
